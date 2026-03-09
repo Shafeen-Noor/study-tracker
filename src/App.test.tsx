a50dd790f8467cfe48eb764a573797d10537e542
Shafeen Noor <72748751+Shafeen-Noor@users.noreply.github.com>
2026-03-09 21:49:50 +0100

    Test commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,7 +3,7 @@ import { MemoryRouter } from "react-router-dom"
 import App from "./App"
 
 describe("App", () => {
-  it("smoke: renders without crashing", () => {
+  it("smoke: App renders without crashing", () => {
     render(
       <MemoryRouter>
         <App />
